af4a405f6beb1e4458a6dc57919378e89a44a040
jeonghoon-tube <shu7081@gmail.com>
2023-05-05 14:16:41 +0900

    tailwindCSS 추가

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="m-3 bg-blue-500 text-green-300">React</h1>
+      <p>How to setting the project</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.cofnig.js
+
+module.exports = {
+  content: ["./src/**/*.jsx", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
