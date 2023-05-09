327a023c105a1f0f67cafd0ddb8e04f18a54ce5c
v-va <36254544+v-va@users.noreply.github.com>
2023-05-10 07:33:34 +0900

    #16 contents 기능 구현 (#22)

--- src/pages/Contents/Contents.jsx
+++ src/pages/Contents/Contents.jsx
@@ -1,157 +1,55 @@
 import React from 'react';
 import styles from './Contents.module.css';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getPosts } from '../../api/posts';
 
 const Contents = () => {
+  const { isLoading, isError, data } = useQuery('posts', getPosts);
+
+  if (isLoading) {
+    return <div>로딩중입니다...</div>;
+  }
+  if (isError) {
+    return <div>오류가 발생하였습니다.</div>;
+  }
+
   return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles.wrap}`}>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photoWrap}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/6dTBQAAJW6QLbLUaneCZWL3Vhf4.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>
-              제주도제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도
-              좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photo}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://s3.ap-northeast-2.amazonaws.com/cloimage/home/rails/clo/public/ckeditor_assets/pictures/1611/content_1.bmp"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>제주도제주도 ㅋㅋㅋㅋㅋㅋ</div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
+    <>
+      {data.data ? (
+        <div className={`${styles.container}`}>
+          <div className={`${styles.wrap}`}>
+            {data.data.map((post) => {
+              console.log(post)
+              return (
+                <Link to={`/details/${post.postId}`} key={post.postId}>
+                  <div className={`${styles.cardWrap}`}>
+                    <div className={`${styles.photoWrap}`}>
+                      <img
+                        className={`${styles.photo}`}
+                        src={post.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className={`${styles.contentsWrap}`}>
+                      <div className={`${styles.likes} text-lg font-bold`}>
+                        <span>❤ {post.likes}</span>
+                      </div>
+                      <div className={`${styles.title} text-2xl font-bold`}>
+                        {post.title}
+                      </div>
+                      <div className={`${styles.contents}`}>
+                        {post.subject}
+                      </div>
+                    </div>
+                  </div>
+            </Link>
+              );
+            })}
           </div>
         </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photo}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://www.greenkorea.org/wp-content/uploads/2022/04/20220404_081425.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>제주도제주도 ㅋㅋㅋㅋㅋㅋ</div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photo}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202211/05/30f53943-37f8-40e4-9360-3c70f4d4a0b6.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>제주도제주도 ㅋㅋㅋㅋㅋㅋ</div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photo}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://www.headlinejeju.co.kr/news/photo/201812/351508_248953_1109.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>제주도제주도 ㅋㅋㅋㅋㅋㅋ</div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photo}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://www.horsepia.com/hp/cms//images/sub/horse_pasture_01_220617_mo.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>제주도제주도 ㅋㅋㅋㅋㅋㅋ</div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photoWrap}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/6dTBQAAJW6QLbLUaneCZWL3Vhf4.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>
-              제주도제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도
-              좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.cardWrap}`}>
-          <div className={`${styles.photoWrap}`}>
-            <img
-              className={`${styles.photo}`}
-              src="https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/6dTBQAAJW6QLbLUaneCZWL3Vhf4.jpg"
-              alt=""
-            />
-          </div>
-          <div className={`${styles.contentsWrap}`}>
-            <div className={`${styles.title}`}>
-              제주도제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도
-              좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-            <div className={`${styles.contents}`}>
-              제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ제주도 좋아요
-              ㅋㅋㅋㅋㅋㅋ제주도 좋아요 ㅋㅋㅋㅋㅋㅋ
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 };
 
