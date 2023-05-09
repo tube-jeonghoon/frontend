2a5954a53302f871e311fee2beb7702ff0c2de5f
tube-jeonghoon <55288856+tube-jeonghoon@users.noreply.github.com>
2023-05-10 06:43:59 +0900

    #3 Revert "Feature/layout" (#18)

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
   <div className={`${styles.container} absolute`}>
-    <Link to={'/'}><img src={process.env.PUBLIC_URL + '/logo_bangmoon.png'} alt="logo" /></Link>
+    <img src={process.env.PUBLIC_URL + 'logo_bangmoon.png'} alt="logo" />
     </div>
     );
 };
--- src/components/Layout/Nav/Nav.jsx
+++ src/components/Layout/Nav/Nav.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import styles from './Nav.module.css';
-import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className={`${styles.container} absolute text-white h-full`}>
       {/* ----- MenuContainer ----- */}
       <ul className={`${styles.menuContainer}`}>
-          <li><Link to={'/'}>HOME</Link></li>
+          <li>HOME</li>
           <li>RANKING</li>
           <li>
-            <Link to={'/login'} className={`${styles.login}`}>
+            <div className={`${styles.login}`}>
               <span className={`${styles.text}`}>LOGIN</span>
-            </Link>
+            </div>
           </li>
-          <li className={`${styles.write}`}><Link to={'/posts'}>WRITE</Link></li>
+          <li className={`${styles.write}`}>WRITE</li>
       </ul>
     </nav>
   );
