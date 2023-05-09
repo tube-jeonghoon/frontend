03e242ddc3a730f494d9048657b23b3edee301f6
v-va <poruch0915@gmail.com>
2023-05-10 06:37:36 +0900

    레이아웃 페이지 수정 nav바 링크 달기, header 사진 경로수정

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
   <div className={`${styles.container} absolute`}>
-    <img src={process.env.PUBLIC_URL + 'logo_bangmoon.png'} alt="logo" />
+    <Link to={'/'}><img src={process.env.PUBLIC_URL + '/logo_bangmoon.png'} alt="logo" /></Link>
     </div>
     );
 };
--- src/components/Layout/Nav/Nav.jsx
+++ src/components/Layout/Nav/Nav.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className={`${styles.container} absolute text-white h-full`}>
       {/* ----- MenuContainer ----- */}
       <ul className={`${styles.menuContainer}`}>
-          <li>HOME</li>
+          <li><Link to={'/'}>HOME</Link></li>
           <li>RANKING</li>
           <li>
-            <div className={`${styles.login}`}>
+            <Link to={'/login'} className={`${styles.login}`}>
               <span className={`${styles.text}`}>LOGIN</span>
-            </div>
+            </Link>
           </li>
-          <li className={`${styles.write}`}>WRITE</li>
+          <li className={`${styles.write}`}><Link to={'/posts'}>WRITE</Link></li>
       </ul>
     </nav>
   );
